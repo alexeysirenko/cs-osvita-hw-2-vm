a9fe1c4afaf11322d76b264d71dc0b4bf09d8673
Alex Sirenko <sirenko.alexey@gmail.com>
2025-12-25 13:17:31 +0200

    Reduce the number of 'as usize' casts

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -15,13 +15,13 @@ const R2: usize = 2;
 const MEMORY_SHIFT: usize = 8;
 
 pub fn compute(memory: &mut [u8; 256]) {
-    let mut registers: [u8; 3] = [MEMORY_SHIFT as u8, 0, 0];
+    let mut registers: [usize; 3] = [MEMORY_SHIFT, 0, 0];
 
     loop {
-        let pc = registers[0] as usize;
+        let pc = registers[PC];
         let op = memory[pc];
 
-        let new_pc = match op {
+        registers[PC] = match op {
             HALT => break,
             LOAD => load(memory, &mut registers, pc),
             STORE => store(memory, &registers, pc),
@@ -33,7 +33,6 @@ pub fn compute(memory: &mut [u8; 256]) {
             BEQZ => beqz(memory, &registers, pc),
             _ => panic!("Unknown opcode: {:#04x}", op),
         };
-        registers[PC] = new_pc as u8;
     }
 }
 
@@ -55,64 +54,67 @@ fn get_cmd(memory: &[u8; 256], idx: usize) -> u8 {
     memory[idx]
 }
 
-fn load(memory: &[u8; 256], registers: &mut [u8; 3], cmd_idx: usize) -> usize {
+fn get_idx(memory: &[u8; 256], idx: usize) -> usize {
+    get_cmd(memory, idx) as usize
+}
+
+fn load(memory: &[u8; 256], registers: &mut [usize; 3], cmd_idx: usize) -> usize {
     const CMD_LEN: usize = 3;
-    let reg_idx = get_cmd(memory, cmd_idx + 1) as usize;
-    let mem_addr = get_cmd(memory, cmd_idx + 2) as usize;
+    let reg_idx = get_idx(memory, cmd_idx + 1);
+    let mem_addr = get_idx(memory, cmd_idx + 2);
     assert!(reg_idx == R1 || reg_idx == R2);
-    registers[reg_idx] = get_mem(memory, mem_addr);
+    registers[reg_idx] = get_mem(memory, mem_addr) as usize;
     cmd_idx + CMD_LEN
 }
 
-fn store(memory: &mut [u8; 256], registers: &[u8; 3], cmd_idx: usize) -> usize {
+fn store(memory: &mut [u8; 256], registers: &[usize; 3], cmd_idx: usize) -> usize {
     const CMD_LEN: usize = 3;
-    let reg_idx = get_cmd(memory, cmd_idx + 1) as usize;
-    let mem_addr = get_cmd(memory, cmd_idx + 2) as usize;
+    let reg_idx = get_idx(memory, cmd_idx + 1);
+    let mem_addr = get_idx(memory, cmd_idx + 2);
     assert!(reg_idx == R1 || reg_idx == R2);
-    set_mem(memory, mem_addr, registers[reg_idx]);
+    set_mem(memory, mem_addr, registers[reg_idx] as u8);
     cmd_idx + CMD_LEN
 }
 
 fn arithmetic_op(
     memory: &[u8; 256],
-    registers: &mut [u8; 3],
+    registers: &mut [usize; 3],
     cmd_idx: usize,
     op: fn(u8, u8) -> u8,
 ) -> usize {
     const CMD_LEN: usize = 3;
-    let reg1 = get_cmd(memory, cmd_idx + 1) as usize;
-    let reg2 = get_cmd(memory, cmd_idx + 2) as usize;
+    let reg1 = get_idx(memory, cmd_idx + 1);
+    let reg2 = get_idx(memory, cmd_idx + 2);
     assert!(reg1 == R1 || reg1 == R2);
     assert!(reg2 == R1 || reg2 == R2);
-    registers[reg1] = op(registers[reg1], registers[reg2]);
+    registers[reg1] = op(registers[reg1] as u8, registers[reg2] as u8) as usize;
     cmd_idx + CMD_LEN
 }
 
 fn arithmetic_immediate_op(
     memory: &[u8; 256],
-    registers: &mut [u8; 3],
+    registers: &mut [usize; 3],
     cmd_idx: usize,
     op: fn(u8, u8) -> u8,
 ) -> usize {
     const CMD_LEN: usize = 3;
-    let reg1 = get_cmd(memory, cmd_idx + 1) as usize;
+    let reg1 = get_idx(memory, cmd_idx + 1);
     let value = get_cmd(memory, cmd_idx + 2);
     assert!(reg1 == R1 || reg1 == R2);
-    registers[reg1] = op(registers[reg1], value);
+    registers[reg1] = op(registers[reg1] as u8, value) as usize;
     cmd_idx + CMD_LEN
 }
 
 fn jump(memory: &[u8; 256], cmd_idx: usize) -> usize {
-    let jump_to = get_cmd(memory, cmd_idx + 1) as usize;
+    let jump_to = get_idx(memory, cmd_idx + 1);
     assert!(jump_to >= MEMORY_SHIFT && jump_to < memory.len());
     jump_to
 }
 
-// branch if equa to zero is an offset, not absolute value
-fn beqz(memory: &[u8; 256], registers: &[u8; 3], cmd_idx: usize) -> usize {
+fn beqz(memory: &[u8; 256], registers: &[usize; 3], cmd_idx: usize) -> usize {
     const CMD_LEN: usize = 3;
-    let reg1 = get_cmd(memory, cmd_idx + 1) as usize;
-    let offset = get_cmd(memory, cmd_idx + 2) as usize;
+    let reg1 = get_idx(memory, cmd_idx + 1);
+    let offset = get_idx(memory, cmd_idx + 2);
     assert!(reg1 == R1 || reg1 == R2);
     if registers[reg1] == 0 {
         let jump_to = cmd_idx + CMD_LEN + offset;
